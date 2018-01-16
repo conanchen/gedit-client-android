b4186b4b335e158561c421d930a11c742c9576c2
conanchen <conan8chan@yahoo.com>
2018-01-16 16:30:36 +0800

    整合权限

--- app/src/main/java/com/github/conanchen/gedit/ui/MainActivity.java
+++ app/src/main/java/com/github/conanchen/gedit/ui/MainActivity.java
@@ -1,20 +1,36 @@
 package com.github.conanchen.gedit.ui;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.github.conanchen.gedit.R;
 import com.github.conanchen.gedit.di.common.BaseFragmentActivity;
 import com.github.conanchen.gedit.ui.auth.CurrentSigninViewModel;
 import com.github.conanchen.gedit.ui.my.MySummaryFragment;
 import com.github.conanchen.gedit.ui.store.StoreListFragment;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import javax.inject.Inject;
 
@@ -117,7 +133,67 @@ public class MainActivity extends BaseFragmentActivity {
                         .show(mySummaryFragment)
                         .commit();
 
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Dexter.withActivity(MainActivity.this)
+                                .withPermission(Manifest.permission.CAMERA)
+                                .withListener(new PermissionListener() {
+                                    @Override
+                                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                                        ARouter.getInstance().build("/app/GaptureActivity").navigation();
+                                    }
+
+                                    @Override
+                                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                                        Toast.makeText(MainActivity.this,"我们需要摄像头访问权限来启动扫码功能，can not open GaptureActivity ",Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                        MainActivity.this.showPermissionRationale(token);
+                                    }
+                                })
+                                .withErrorListener(new PermissionRequestErrorListener() {
+                                    @Override
+                                    public void onError(DexterError error) {
+
+                                    }
+                                })
+                                .onSameThread()
+                                .check();
+                    }
+                }).start();
+
                 break;
         }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public void showPermissionRationale(final PermissionToken token) {
+        new AlertDialog.Builder(this).setTitle(R.string.permission_rationale_title)
+                .setMessage(R.string.permission_rationale_message)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        token.cancelPermissionRequest();
+                    }
+                })
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        token.continuePermissionRequest();
+                    }
+                })
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        token.cancelPermissionRequest();
+                    }
+                })
+                .show();
     }
 }
